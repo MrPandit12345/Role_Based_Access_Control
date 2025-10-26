7169632705ca766b17c3db5217cfecf08ae7cd16
MrPandit12345 <117350192+MrPandit12345@users.noreply.github.com>
2025-10-26 13:04:34 +0530

    added routes of main file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,12 @@ import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
 import RequireAuth from "./components/RequireAuth";
 
+import Roles from "./components/Role";
+import Users from "./components/Users";
+import Enterprises from "./components/Enterprise";
+import Employees from "./components/Employee";
+import Products from "./components/Product";
+
 export default function App() {
   return (
     <BrowserRouter>
@@ -16,6 +22,46 @@ export default function App() {
           element={
             <RequireAuth>
               <Dashboard />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/roles"
+          element={
+            <RequireAuth>
+              <Roles />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/users"
+          element={
+            <RequireAuth>
+              <Users />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/enterprises"
+          element={
+            <RequireAuth>
+              <Enterprises />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/employees"
+          element={
+            <RequireAuth>
+              <Employees />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/products"
+          element={
+            <RequireAuth>
+              <Products />
             </RequireAuth>
           }
         />
